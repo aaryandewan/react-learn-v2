c9cdea7da7bbb8bc8c9bd835574ad2a394d24df9
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-05 10:19:55 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -4,21 +4,11 @@ import './style.css';
 import { useImmer } from 'use-immer'
 
 export default function App() {
-  const [name, setName] = useState('');
-  const [arrs, setArrs] = useState([]);
-  function handleClick(){
-    setArrs([...arrs, name]);
-    setName('');
-  }
 
 
   return (
     <div>
-      <input value={name} onChange={e => setName(e.target.value)}/>
-      <button onClick={handleClick}>save</button>
-      <ul>
-        {arrs.map(item => {return <li>{item}</li>})}
-      </ul>
+
       <h1>Aaryan D</h1>
     </div>
   );
