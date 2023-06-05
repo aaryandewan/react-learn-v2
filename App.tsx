925f021deee956f961709a8b518111d019866aea
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-05 09:49:56 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import './style.css';
+import { useImmer } from 'use-immer'
 
 export default function App() {
+  const [age, setAge] = React.useState(0);
+  const [obj, setObj] = useImmer({name: "Aaryan", age: 23});
+  function changeAge(){
+    setAge(age + 1);
+  }
+
+  function changeObj(){
+    setObj(draft => {draft.name = "mehak"});
+  }
   return (
     <div>
+      <button onClick={changeAge}>Click</button>
+      {age}
+      {obj.name}
+      <button onClick={changeObj}>z</button>
       <h1>Aaryan D</h1>
     </div>
   );
