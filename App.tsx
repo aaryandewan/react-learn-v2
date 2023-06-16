4dad7a3fed3fdc10fd2ba141c8c7259ba3c81ece
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-16 15:10:38 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import {useState} from 'react';
 import './style.css';
 import { useImmer } from 'use-immer'
+import {store} from './app/';
+import { Provider } from 'react-redux'
+
 
 export default function App() {
 
